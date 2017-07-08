99f78826bc6c002cb550b81424a93ccd5b1c610a
michal0x17 <michal0x17@gmail.com>
2017-07-08 09:49:35 +0000

    add nsb endpoint configuration

---
+++ src/src/Web/App_Start/EndpointConfig.cs
@@ -0,0 +1,39 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using NServiceBus;
+using System.Web.Mvc;
+
+namespace Web
+{
+    public class EndpointConfig
+    {
+        public static void RegisterEndpoint(IEndpointInstance endpoint)
+        {
+            var builder = new ContainerBuilder();
+
+            // Register the MVC controllers.
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            // Register the endpoint as a factory as the instance isn't created yet.
+            builder.Register(ctx => endpoint).SingleInstance();
+
+            var container = builder.Build();
+
+            // Set the dependency resolver to be Autofac.
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            var endpointConfiguration = new EndpointConfiguration("blogcomments");
+            endpointConfiguration.MakeInstanceUniquelyAddressable("1");
+            endpointConfiguration.EnableCallbacks();
+            endpointConfiguration.UseSerialization<JsonSerializer>();
+            endpointConfiguration.UseContainer<AutofacBuilder>(
+                customizations: customizations =>
+                {
+                    customizations.ExistingLifetimeScope(container);
+                });
+            endpointConfiguration.UsePersistence<LearningPersistence>();
+            endpointConfiguration.UseTransport<LearningTransport>();
+
+            endpoint = Endpoint.Start(endpointConfiguration).GetAwaiter().GetResult();
+        }
+    }
+}
--- src/src/Web/Global.asax.cs
+++ src/src/Web/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NServiceBus;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,12 +11,20 @@ namespace Web
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private IEndpointInstance endpoint;
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            EndpointConfig.RegisterEndpoint(this.endpoint);
+        }
+
+        protected void Application_End()
+        {
+            endpoint?.Stop().GetAwaiter().GetResult();
         }
     }
 }
