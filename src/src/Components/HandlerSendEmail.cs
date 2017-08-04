f4a9996a4a653d4d73375b793251fc4cd4da1aaa
michal0x17 <michal0x17@gmail.com>
2017-08-04 13:43:03 +0200

    add handler base structure

---
+++ src/src/Components/HandlerSendEmail.cs
@@ -0,0 +1,19 @@
+﻿using Messages.Commands;
+using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Components
+{
+    public class HandlerSendEmail : IHandleMessages<SendEmail>
+    {
+        public Task Handle(SendEmail message, IMessageHandlerContext context)
+        {
+            ///TODO: to implement
+            return Task.CompletedTask;
+        }
+    }
+}
