661deb34f369c52ed74d34414eec7344c263e596
michal0x17 <michal0x17@gmail.com>
2017-08-13 19:21:27 +0200

    add test assertions

--- src/tests/integration/Web.Integration.Tests/CommentControllerTests.cs
+++ src/tests/integration/Web.Integration.Tests/CommentControllerTests.cs
@@ -1,17 +1,22 @@
 ﻿namespace Web.Integration.Tests
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Net.Http;
     using System.Text;
     using System.Threading.Tasks;
     using System.Web.Script.Serialization;
     using NUnit.Framework;
+    using Simple.Data;
     using Web.Models;
 
     [TestFixture]
     ////[Ignore("do not auto run")]
     public class CommentControllerTests
     {
+        private IConfigurationManager configurationManager = new ConfigurationManager();
+
         [Test]
         public async Task Post_ForCommentData_FullProcessShouldBeSuccess()
         {
@@ -23,7 +28,6 @@
             var serializer = new JavaScriptSerializer();
             var json = serializer.Serialize(comment);
             var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
-            ////return await client.PostAsync(requestUrl, stringContent);
 
             // Act
             HttpResponseMessage response = await client.PostAsync("Comment/RequestForComment", stringContent);
@@ -37,7 +41,28 @@
             {
                 Console.WriteLine(ex);
                 Assert.True(false);
+                return;
             }
+
+            await Task.Delay(15000).ConfigureAwait(false);
+
+            List<ResultRow> result = Database.OpenConnection(this.configurationManager.NsbTransportConnectionString)
+                    .SagaTestResults
+                    .All()
+                    .ToList<ResultRow>();
+
+            Assert.NotNull(result.FirstOrDefault(row => row.Result == 1));
+            Assert.NotNull(result.FirstOrDefault(row => row.Result == 2));
+            Assert.NotNull(result.FirstOrDefault(row => row.Result == 3));
+            Assert.NotNull(result.FirstOrDefault(row => row.Result == 4));
+            Assert.NotNull(result.FirstOrDefault(row => row.Result == 5));
+        }
+
+        public class ResultRow
+        {
+            public int Id { get; set; }
+
+            public int Result { get; set; }
         }
     }
 }
