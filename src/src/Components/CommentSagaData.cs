47eb4a4aa197156a7894daf3b49490d5e9a6926a
michal0x17 <michal0x17@gmail.com>
2017-08-15 21:20:37 +0200

    small refactoring

--- src/src/Components/CommentSagaData.cs
+++ src/src/Components/CommentSagaData.cs
@@ -7,15 +7,15 @@
     {
         public Guid CommentId { get; set; }
 
-        public string FileName { get; set; }
-
-        public string Content { get; set; }
-
         public string UserName { get; set; }
 
         public string UserEmail { get; set; }
 
         public string UserWebsite { get; set; }
+
+        public string FileName { get; set; }
+
+        public string Content { get; set; }
 
         public string BranchName { get; set; }
     }
