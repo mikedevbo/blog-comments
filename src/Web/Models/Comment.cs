3bf4ba67bd7cefdc90cc4b84d01c4da592b340eb
michal0x17 <michal0x17@gmail.com>
2017-12-13 21:06:39 +0100

    upgrade Web.Integration.Tests project

---
+++ src/Web/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿namespace Web.Models
+{
+    public class Comment
+    {
+        public string UserName { get; set; }
+
+        public string UserEmail { get; set; }
+
+        public string UserWebsite { get; set; }
+
+        public string FileName { get; set; }
+
+        public string Content { get; set; }
+    }
+}
--- src/Web/Modules/CommentModule.cs
+++ src/Web/Modules/CommentModule.cs
@@ -1,7 +1,11 @@
 ﻿namespace Web.Modules
 {
+    using System;
+    using Messages.Commands;
     using Nancy;
+    using Nancy.ModelBinding;
     using NServiceBus;
+    using Web.Models;
 
     public class CommentModule : NancyModule
     {
@@ -12,15 +16,23 @@
         {
             this.messageSession = messageSession;
 
-            this.Get["/"] = r => "aaa";
+            this.Get["/"] = r => "test";
 
-            // this.Get["/", true] = async (r, c) =>
-            // {
-            //    var message = new MyMessage();
-            //    await messageSession.Send(message)
-            //        .ConfigureAwait(false);
-            //    return "Message sent to endpoint";
-            // };
+            this.Post["/", true] = async (r, c) =>
+            {
+                var comment = this.Bind<Comment>();
+                await this.messageSession.Send<StartAddingComment>(command =>
+                {
+                    command.CommentId = Guid.NewGuid();
+                    command.UserName = comment.UserName;
+                    command.UserEmail = comment.UserEmail;
+                    command.UserWebsite = comment.UserWebsite;
+                    command.FileName = comment.FileName;
+                    command.Content = comment.Content;
+                }).ConfigureAwait(false);
+
+                return HttpStatusCode.OK;
+            };
         }
     }
 }
