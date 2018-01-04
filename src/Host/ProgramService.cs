ba2e71f8da37da0208d829952601e08c0862cec8
michal0x17 <michal0x17@gmail.com>
2018-01-04 10:42:05 +0100

    add host project

---
+++ src/Host/ProgramService.cs
@@ -0,0 +1,53 @@
+﻿namespace Host
+{
+    using System;
+    using System.ServiceProcess;
+    using System.Threading.Tasks;
+    using NServiceBus;
+
+    public class ProgramService : ServiceBase
+    {
+        private IEndpointInstance endpointInstance;
+
+        public static void Main()
+        {
+            using (var service = new ProgramService())
+            {
+                if (ServiceHelper.IsService())
+                {
+                    Run(service);
+                    return;
+                }
+
+                Console.Title = "Host";
+                service.OnStart(null);
+
+                Console.WriteLine("Endpoint started. Press any key to exit");
+                Console.ReadKey();
+
+                service.OnStop();
+            }
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            this.AsyncOnStart().GetAwaiter().GetResult();
+        }
+
+        protected async Task AsyncOnStart()
+        {
+            var endpointConfiguration = new EndpointConfiguration("Samples.WindowsServiceAndConsole");
+            endpointConfiguration.SendFailedMessagesTo("error");
+            endpointConfiguration.UsePersistence<LearningPersistence>();
+            endpointConfiguration.UseTransport<LearningTransport>();
+            endpointConfiguration.EnableInstallers();
+            this.endpointInstance = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+        }
+
+        protected override void OnStop()
+        {
+            this.endpointInstance?.Stop().GetAwaiter().GetResult();
+        }
+    }
+}
---
+++ src/Host/ServiceHelper.cs
@@ -0,0 +1,87 @@
+﻿namespace Host
+{
+#pragma warning disable SA1307 // AccessibleFieldsMustBeginWithUpperCaseLetter
+#pragma warning disable SA1201 // ElementsMustAppearInTheCorrectOrder
+#pragma warning disable SA1310 // FieldNamesMustNotContainUnderscore
+#pragma warning disable SA1306 // FieldNamesMustBeginWithLowerCaseLetter
+
+    using System;
+    using System.Diagnostics;
+    using System.Runtime.InteropServices;
+
+    public static class ServiceHelper
+    {
+        public static bool IsService()
+        {
+            using (var process = GetParent(Process.GetCurrentProcess()))
+            {
+                return process != null && process.ProcessName == "services";
+            }
+        }
+
+        public static Process GetParent(Process child)
+        {
+            var parentId = 0;
+
+            var handle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+            if (handle == IntPtr.Zero)
+            {
+                return null;
+            }
+
+            var processInfo = new PROCESSENTRY32
+            {
+                dwSize = (uint)Marshal.SizeOf(typeof(PROCESSENTRY32))
+            };
+
+            if (!Process32First(handle, ref processInfo))
+            {
+                return null;
+            }
+
+            do
+            {
+                if (child.Id == processInfo.th32ProcessID)
+                {
+                    parentId = (int)processInfo.th32ParentProcessID;
+                }
+            }
+            while (parentId == 0 && Process32Next(handle, ref processInfo));
+
+            if (parentId > 0)
+            {
+                return Process.GetProcessById(parentId);
+            }
+
+            return null;
+        }
+
+        private static uint TH32CS_SNAPPROCESS = 2;
+
+        [DllImport("kernel32.dll")]
+        public static extern bool Process32Next(IntPtr hSnapshot, ref PROCESSENTRY32 lppe);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr CreateToolhelp32Snapshot(uint dwFlags, uint th32ProcessID);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool Process32First(IntPtr hSnapshot, ref PROCESSENTRY32 lppe);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESSENTRY32
+        {
+            public uint dwSize;
+            public uint cntUsage;
+            public uint th32ProcessID;
+            public IntPtr th32DefaultHeapID;
+            public uint th32ModuleID;
+            public uint cntThreads;
+            public uint th32ParentProcessID;
+            public int pcPriClassBase;
+            public uint dwFlags;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string szExeFile;
+        }
+    }
+}
