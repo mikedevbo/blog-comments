33ff2629741845049836b84a735919ebf65f4f6e
michal0x17 <michal0x17@gmail.com>
2018-05-02 12:14:25 +0200

    add script of building solution

---
+++ src/Deployment/build.ps1
@@ -0,0 +1,39 @@
+﻿[CmdletBinding()]
+Param(
+
+    [Parameter(Mandatory=$True)]
+    [string]$solutionPath,
+
+    [Parameter(Mandatory=$True)]
+    [string]$binPath,
+
+    [Parameter(Mandatory=$True)]
+    [string]$msbuildPath
+
+)
+
+$ErrorActionPreference = "Stop"
+
+try
+{
+    Write-Host 'clean bin directory'
+    Remove-Item "$binPath\*" -Recurse -Force
+
+    Write-Host 'build solution'
+    $buildLogFile = "$binPath\build.log"
+    $run = $msbuildPath
+    $p1 = $solutionPath
+    $p2 = "/p:Configuration=Release"
+    $p3 = "/flp:logfile=$buildLogFile"
+
+    & $run $p1 $p2 $p3
+
+    if(!$?)
+    {
+        throw "Buit failed see $buildLogFile for details"
+    }
+}
+catch [System.Exception]
+{
+    throw;
+}
