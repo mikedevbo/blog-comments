7de76780afcebd21000e1bf2784f7f3196984a9a
michal0x17 <michal0x17@gmail.com>
2018-05-31 20:37:37 +0200

    add functionality of cloning git repository

--- src/Deployment/build.ps1
+++ src/Deployment/build.ps1
@@ -2,26 +2,40 @@
 Param(
 
     [Parameter(Mandatory=$True)]
+    [string]$gitExePath,
+    
+    [Parameter(Mandatory=$True)]
+    [string]$nugetExePath,
+
+    [Parameter(Mandatory=$True)]
+    [string]$msbuildExePath,
+
+    [Parameter(Mandatory=$True)]
+    [string]$buildArtifactsPath,
+
+    [Parameter(Mandatory=$True)]
     [string]$solutionPath,
 
     [Parameter(Mandatory=$True)]
-    [string]$binPath,
-
-    [Parameter(Mandatory=$True)]
-    [string]$msbuildPath
-
+    [string]$binPath
 )
 
 $ErrorActionPreference = "Stop"
 
 try
 {
-    Write-Host "clean bin directory"
-    Remove-Item "$binPath\*" -Recurse -Force
+    Write-Host "clean artifacts directory"
+    Remove-Item "$buildArtifactsPath\*" -Recurse -Force
+    
+    Write-Host "clone repository"
+    & $gitExePath "clone" "-q" "https://github.com/mikedevbo/blog-comments.git" "$buildArtifactsPath"
+    
+    Write-Host "restore nuget packages"
+    & $nugetExePath "restore" "$solutionPath"
 
     Write-Host "build solution"
     $buildLogFile = "$binPath\build.log"
-    $run = $msbuildPath
+    $run = $msbuildExePath
     $p1 = $solutionPath
     $p2 = "/p:Configuration=Release"
     $p3 = "/flp:logfile=$buildLogFile"
