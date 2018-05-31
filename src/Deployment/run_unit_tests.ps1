2d27f441fbe9f56916d402d2185497df9e757664
michal0x17 <michal0x17@gmail.com>
2018-05-31 09:05:05 +0200

    add script of running unit tests

---
+++ src/Deployment/run_unit_tests.ps1
@@ -0,0 +1,23 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True)]
+    [string]$binPath,
+
+    [Parameter(Mandatory=$True)]
+    [string]$NunitExePath
+)
+
+$ErrorActionPreference = "Stop"
+
+#main
+
+try
+{
+    $tests = (Get-ChildItem $binPath -Recurse -Include *unit.tests.dll)
+
+    & $NunitExePath $tests --noheader --work=$binPath
+}
+catch
+{
+    throw;
+}
