a95cddec688ee3f8ff20772c8103112ce82cec88
michal0x17 <michal0x17@gmail.com>
2018-08-23 21:14:50 +0200

    remove unused code

--- src/Components/GitHub/GitHubApiForTests.cs
+++ src/Components/GitHub/GitHubApiForTests.cs
@@ -20,10 +20,6 @@
 
         public Task<string> GetSha(string userAgent, string authorizationToken, string repositoryName, string branchName)
         {
-            Database.OpenConnection(this.configurationManager.NsbTransportConnectionString)
-                    .SagaTestResults
-                    .Insert(Result: 3);
-
             log.Info("GetRepository");
 
             return Task.Run(() => @"1234");
@@ -31,10 +27,6 @@
 
         public Task CreateRepositoryBranch(string userAgent, string authorizationToken, string repositoryName, string masterBranchName, string newBranchName)
         {
-            Database.OpenConnection(this.configurationManager.NsbTransportConnectionString)
-                    .SagaTestResults
-                    .Insert(Result: 2);
-
             log.Info("CreateRepositoryBranch");
 
             return Task.CompletedTask;
@@ -42,10 +34,6 @@
 
         public Task UpdateFile(string userAgent, string authorizationToken, string repositoryName, string branchName, string fileName, string content)
         {
-            Database.OpenConnection(this.configurationManager.NsbTransportConnectionString)
-                    .SagaTestResults
-                    .Insert(Result: 4);
-
             log.Info("UpdateFile");
 
             return Task.CompletedTask;
@@ -53,10 +41,6 @@
 
         public Task<string> CreatePullRequest(string userAgent, string authorizationToken, string repositoryName, string headBranchName, string baseBranchName)
         {
-            Database.OpenConnection(this.configurationManager.NsbTransportConnectionString)
-                    .SagaTestResults
-                    .Insert(Result: 1);
-
             log.Info("CreatePullRequest");
 
             return Task.Run(() => @"https://test/test");
@@ -64,10 +48,6 @@
 
         public Task<(bool result, string etag)> IsPullRequestOpen(string userAgent, string authorizationToken, string pullRequestUrl, string etag)
         {
-            Database.OpenConnection(this.configurationManager.NsbTransportConnectionString)
-                    .SagaTestResults
-                    .Insert(Result: 6);
-
             log.Info("IsPullRequestOpen");
 
             return Task.Run(() => (false, "1234"));
@@ -75,10 +55,6 @@
 
         public Task<bool> IsPullRequestMerged(string userAgent, string authorizationToken, string pullRequestUrl)
         {
-            Database.OpenConnection(this.configurationManager.NsbTransportConnectionString)
-                    .SagaTestResults
-                    .Insert(Result: 7);
-
             log.Info("IsPullRequestMerged");
 
             return Task.Run(() => true);
