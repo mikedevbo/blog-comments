368f18ffed5aef6c3e35868fc81a7b72b88dfe8a
michal0x17 <michal0x17@gmail.com>
2018-08-23 20:34:12 +0200

    test change

--- src/Components/HandlerSendEmail.cs
+++ src/Components/HandlerSendEmail.cs
@@ -49,7 +49,7 @@
                 return string.Empty;
             }
 
-            // Depend on Jekyll file format
+            // Depend on Jekyll file format -> test
             var s = fileName.Split('-');
 
             return string.Format(
