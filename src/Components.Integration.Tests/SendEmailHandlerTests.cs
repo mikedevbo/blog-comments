1137623bc51c1deba1d596fbb43f16e249d36fc5
michal0x17 <michal0x17@gmail.com>
2018-10-14 09:27:05 +0200

    move tests

--- src/Components.Integration.Tests/SendEmailHandlerTests.cs
+++ src/Components.Integration.Tests/SendEmailHandlerTests.cs
@@ -1,40 +1,11 @@
 ﻿namespace Components.Integration.Tests
 {
-    using System;
-    using Messages;
     using NUnit.Framework;
 
     [TestFixture]
     [Ignore("only for manual tests")]
     public class SendEmailHandlerTests
     {
-        [TestCase(CommentAnswerStatus.Approved)]
-        [TestCase(CommentAnswerStatus.Rejected)]
-        public void GetSubject_Execute_ShowResult(CommentAnswerStatus responseStatus)
-        {
-            // Arrange
-            var handler = this.GetHandlerSetEmail();
-
-            // Act
-            var result = handler.GetSubject(responseStatus);
-
-            // Assert
-            Console.WriteLine(result);
-        }
-
-        [Test]
-        public void GetBody_Execute_ShowResult()
-        {
-            // Arrange
-            var handler = this.GetHandlerSetEmail();
-
-            // Act
-            var result = handler.GetBody("someBlogDomainName", "2018-02-10-someFileName.md", CommentAnswerStatus.Approved);
-
-            // Assert
-            Console.WriteLine(result);
-        }
-
         private SendEmailHandler GetHandlerSetEmail()
         {
             return new SendEmailHandler(new Common.ConfigurationManager());
---
+++ src/Components.Unit.Tests/SendEmailHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿namespace Components.Unit.Tests
+{
+    using System;
+    using Common;
+    using Messages;
+    using NSubstitute;
+    using NUnit.Framework;
+
+    public class SendEmailHandlerTests
+    {
+        private IConfigurationManager configurationManager;
+
+        [TestCase(CommentAnswerStatus.Approved)]
+        [TestCase(CommentAnswerStatus.Rejected)]
+        public void GetSubject_Execute_ShowResult(CommentAnswerStatus responseStatus)
+        {
+            // Arrange
+            var handler = this.GetHandlerSetEmail();
+
+            // Act
+            var result = handler.GetSubject(responseStatus);
+
+            // Assert
+            Console.WriteLine(result);
+        }
+
+        [Test]
+        public void GetBody_Execute_ShowResult()
+        {
+            // Arrange
+            var handler = this.GetHandlerSetEmail();
+
+            // Act
+            var result = handler.GetBody("someBlogDomainName", "2018-02-10-someFileName.md", CommentAnswerStatus.Approved);
+
+            // Assert
+            Console.WriteLine(result);
+        }
+
+        private SendEmailHandler GetHandlerSetEmail()
+        {
+            this.configurationManager = Substitute.For<IConfigurationManager>();
+
+            return new SendEmailHandler(this.configurationManager);
+        }
+    }
+}
