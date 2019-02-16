a5a402a9cd5e5f0aa45468fcf6d3674f35045920
michal0x17 <michal0x17@gmail.com>
2019-02-16 15:50:03 +0100

    replace msbuild by dotnet

--- src/Deployment/build.ps1
+++ src/Deployment/build.ps1
@@ -3,12 +3,9 @@ Param(
 
     [Parameter(Mandatory=$True)]
     [string]$gitExePath,
-    
-    [Parameter(Mandatory=$True)]
-    [string]$nugetExePath,
 
     [Parameter(Mandatory=$True)]
-    [string]$msbuildExePath,
+    [string]$dotnetExePath,
 
     [Parameter(Mandatory=$True)]
     [string]$buildArtifactsPath,
@@ -17,37 +14,79 @@ Param(
     [string]$gitRepositoryUrl,	
 	
     [Parameter(Mandatory=$True)]
-    [string]$solutionRelativePath,
-
-    [Parameter(Mandatory=$True)]
-    [string]$binRelativePath
+    [string]$solutionRelativePath
 )
 
 $ErrorActionPreference = "Stop"
 
+function cloneRepository(
+    $git,
+	$repository,
+	$destination)
+{
+    #Write-Host "clean artifacts directory"
+    #Remove-Item "$destination\*" -Recurse -Force
+    
+    #Write-Host "clone repository"
+    #& $git "clone" "-q" $repository $destination
+}
+
+function buildSolution(
+	$dotnet,
+	$destination)
+{
+	& $dotnet build $destination --configuration Release
+	if ($lastexitcode -ne 0)
+	{
+		throw $errorMessage
+	}
+}
+
+function runUnitTests(
+	$dotnet,
+	$destination)
+{
+	& $dotnet test $destination --no-build --configuration Release
+	if ($lastexitcode -ne 0)
+	{
+		throw $errorMessage
+	}
+}
+
+function publishrtifacts(
+	$dotnet,
+	$destination)
+{
+	& $dotnet publish $destination --no-build --configuration Release
+	if ($lastexitcode -ne 0)
+	{
+		throw $errorMessage
+	}
+}
+
+#main
+
 try
 {
-    Write-Host "clean artifacts directory"
-    Remove-Item "$buildArtifactsPath\*" -Recurse -Force
+    ##Write-Host "clean artifacts directory"
+    ##Remove-Item "$buildArtifactsPath\*" -Recurse -Force
     
-    Write-Host "clone repository"
-    & $gitExePath "clone" "-q" $gitRepositoryUrl $buildArtifactsPath
-    
-    Write-Host "restore nuget packages"
-    & $nugetExePath "restore" "$buildArtifactsPath\$solutionRelativePath"
+    ##Write-Host "clone repository"
+    ##& $gitExePath "clone" "-q" $gitRepositoryUrl $buildArtifactsPath
+	
+	#cloneRepository $buildArtifactsPath $gitRepositoryUrl $gitExePath
+ 
+    Write-Host "start build"
+	
+	$path = "$buildArtifactsPath\$solutionRelativePath"
 
-    Write-Host "build solution"
-    $buildLogFile = "$buildArtifactsPath\$binRelativePath\build.log"
-    $run = $msbuildExePath
-    $p1 = "$buildArtifactsPath\$solutionRelativePath"
-    $p2 = "/p:Configuration=Release"
-    $p3 = "/flp:logfile=$buildLogFile"
-
-    & $run $p1 $p2 $p3
+	buildSolution $dotnetExePath $path
+	runUnitTests $dotnetExePath $path
+	publishrtifacts $dotnetExePath $path
 
     if(!$?)
     {
-        throw "Buit failed see $buildLogFile for details"
+        throw $errorMessage
     }
 }
 catch
