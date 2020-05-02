6b787c9180e8a3c69f4a59193f2b7b81dcf13380
mikedevbo <mike@devbo.pl>
2020-05-02 12:10:54 +0200

    move logic to new project

--- src/common/Bc.Common.Logic/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace Bc.Common.Logic
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
--- src/endpoint/Bc.Logic.Endpoint.Integration.Tests/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace Bc.Logic.Endpoint.Integration.Tests
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
