b899955e31cae59d4c0dd3656fbca2021a6f66f7
mikedevbo <mike@devbo.pl>
2020-07-25 12:36:37 +0200

    add unit tests

--- src/tests/unit/Bc.Endpoint.Tests/CommentAnswerNotificationTests.fs
+++ src/tests/unit/Bc.Endpoint.Tests/CommentAnswerNotificationTests.fs
@@ -3,8 +3,10 @@ module Bc.Endpoint.Tests.CommentAnswerNotification
 open System
 open Bc.Contracts.Externals.Endpoint.CommentAnswer.Events
 open Bc.Contracts.Internals.Endpoint.CommentAnswerNotification.Commands
+open Bc.Contracts.Internals.Endpoint.CommentAnswerNotification.Logic
 open Bc.Endpoint
 open NServiceBus.Testing
+open NSubstitute
 open NUnit.Framework
 
 let getContext() =
@@ -55,4 +57,59 @@ module CommentAnswerNotificationEventSubscribingPolicyTests =
         Assert.That(sentMessage.CommentId, Is.EqualTo(commentId))
         Assert.That(sentMessage.IsApproved, Is.False)
 
+module CommentAnswerNotificationPolicyTests =
+
+    let logic = Substitute.For<ICommentAnswerNotificationPolicyLogic>()
+
+    let getPolicy data =
+        CommentAnswerNotificationPolicy(logic, Data = data)
+
+    [<Test>]
+    let Handle_RegisterCommentNotification_ProperResult () =
+
+        // Arrange
+        let commentId = Guid.NewGuid()
+        let userEmail = "sample_user_email"
+        let articleFileName = "sample_file_name"
+        let message = RegisterCommentNotification(commentId, userEmail, articleFileName)
+
+        let policyData = CommentAnswerNotificationPolicy.PolicyData()
+        let policy = getPolicy policyData
+
+        let context = getContext ()
+
+        // Act
+        policy.Handle(message, context) |> ignore
+
+        // Assert
+        let sentNumberOfMessages = context.SentMessages.Length
+
+        Assert.That(sentNumberOfMessages, Is.EqualTo(0))
+        Assert.That(policyData.UserEmail, Is.EqualTo(userEmail))
+        Assert.That(policyData.ArticleFileName, Is.EqualTo(articleFileName))
+
+    [<TestCase(false)>]
+    [<TestCase(true)>]
+    let Handle_CheckCommentAnswer_ProperResult isApproved =
+
+        // Arrange
+        let commentId = Guid.NewGuid()
+        let userEmail = "sample_user_email"
+        let message = NotifyAboutCommentAnswer(commentId, isApproved)
+
+        (logic.IsSendNotification message userEmail).Returns(isApproved) |> ignore
+
+        let policyData = CommentAnswerNotificationPolicy.PolicyData(UserEmail = userEmail)
+        let policy = getPolicy policyData
+
+        let context = getContext ()
+
+        // Act
+        policy.Handle(message, context) |> ignore
+
+        // Assert
+        let sentNumberOfMessages = context.SentMessages.Length
+
+        Assert.That((sentNumberOfMessages = 1), Is.EqualTo(isApproved))
+        //Assert.That(policy.Completed, Is.True)
 
