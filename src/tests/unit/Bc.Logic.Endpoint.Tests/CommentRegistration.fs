4cebe5aab7282e3f2bbac087ee9a0402e13fead8
mikedevbo <mike@devbo.pl>
2020-07-29 06:28:24 +0200

    add unit test

--- src/tests/unit/Bc.Logic.Endpoint.Tests/CommentRegistration.fs
+++ src/tests/unit/Bc.Logic.Endpoint.Tests/CommentRegistration.fs
@@ -1,5 +1,6 @@
 module Bc.Logic.Endpoint.Tests.CommentRegistration
 
+open System
 open Bc.Contracts.Internals.Endpoint.CommentRegistration.Logic
 open Bc.Logic.Endpoint.CommentRegistration
 open NUnit.Framework
@@ -19,6 +20,22 @@ module CommentRegistrationPolicyLogicTests =
 
         // Act
         let result = logic.FormatUserName userName userWebsite
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expectedResult))
+
+    [<Test>]
+    let FormatUserComment_Execute_ProperResult() =
+
+        // Arrange
+        let userName = "user_name"
+        let userComment = "user_comment"
+        let commentAddedDate = DateTime(2020, 7, 29, 10, 0, 0)
+        let expectedResult = "begin-user_name-user_comment-2020-07-29 10:00 UTC \n"
+        let logic = getLogic () :> ICommentRegistrationPolicyLogic
+
+        // Act
+        let result = logic.FormatUserComment userName userComment commentAddedDate
 
         // Assert
         Assert.That(result, Is.EqualTo(expectedResult))
